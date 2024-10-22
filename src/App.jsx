d23fce0a14edd1513200a7e073a69d6b242d4e66
Dhanush Shetty <133327430+Dhanus45@users.noreply.github.com>
2024-10-22 16:51:43 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import InlineComponent from "./Component/InlineComponent.JSX"
+import OutlineComponent from "./Component/OutlineComponent.JSX"
+import Todo from "./Component/Todo"
+
+
+function App() {
+
+  return (
+    <div className="App">
+     {/*<Todo/>*/}
+     <InlineComponent/>
+     <OutlineComponent/>
+    </div>
+  )
+}
+
+export default App
